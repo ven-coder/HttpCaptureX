3515e3cbc3a7ea36d01e3219df04f92e661e7357
Levine <1483232332@qq.com>
2021-02-19 16:49:02 +0800

    首次提交

---
+++ app/src/main/java/com/levine/httpcapturedemoX/App.kt
@@ -0,0 +1,11 @@
+package com.levine.httpcapturedemoX
+
+import android.app.Application
+import com.levine.netcaptureX.NCP
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NCP.getInstance(this).init()
+    }
+}
---
+++ app/src/main/java/com/levine/httpcapturedemoX/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.levine.httpcapturedemoX
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blankj.utilcode.util.*
+import com.levine.netcaptureX.NCPInterceptor
+import com.levine.netcaptureX.NetCaptureRecordActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+//import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        LogUtils.getConfig().globalTag = "123456"
+        val request = Request.Builder()
+            .url("https://api.douban.com/v2/book/1220562?aaaaa=bbbbbb")
+            .addHeader("Header-1", "Header1")
+            .addHeader("Header-2", "Header2")
+            .post(
+                FormBody.Builder()
+                    .add("param_1", "param_value_1++dsafaad++--===")
+                    .build()
+            ).build()
+
+        val okHttpClient = OkHttpClient().newBuilder()
+        okHttpClient.addInterceptor(NCPInterceptor(NetDataConvert()))
+        tv_test.setOnClickListener {
+            val call = okHttpClient.build().newCall(request)
+            call.enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    NetCaptureRecordActivity.launch(this@MainActivity)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    NetCaptureRecordActivity.launch(this@MainActivity)
+                }
+            })
+        }
+    }
+}
